e1b0555beea90a652ec92f05b8ceb768a2e6306d
Lang Martin <lang.martin@gmail.com>
2022-04-23 16:53:18 -0400

    need to figure out the whole buffer is in a string

--- c/lexer.c
+++ c/lexer.c
@@ -27,43 +27,133 @@ typedef struct token_list_t token_list;
 
 struct token_t {
   token_type t;
+  int line;
   char *val;
 };
 
 struct token_list_t {
-  token token;
+  token *token;
   token_list *next;
 };
+
+#define READ_BUFFER 1024
+
+typedef struct {
+  char state;
+  char *buffer;
+  int index;
+  int eof;
+  FILE *fh;
+  int line;
+} lexer;
+
+int match(lexer *l, char expected) {
+  if (l->index > READ_BUFFER - 1) {
+    return 0;                   /* FIXME next buffer */
+  }
+
+  if (l->buffer[l->index + 1] == expected) {
+    l->index++;
+    return 1;
+  }
+  return 0;
+}
+
+char peek(lexer *l) {
+  return l->buffer[l->index + 1];
+}
+
+void add_token(token_list *ts, token_type t, int line) {
+  token *tok = malloc(sizeof(token));
+  tok->t = t;
+  tok->line = line;
+
+  token_list *cell = malloc(sizeof(token_list));
+  cell->token = tok;
+  cell->next = ts;
+  ts = cell;
+}
+
+int eof(lexer *l) {
+  return l->eof < READ_BUFFER && l->index == l->eof;
+}
+
+char advance(lexer *l) {
+  int read_count;
+
+  if (eof(l)) return '\0';
+
+  if (l->index >= READ_BUFFER - 1) {
+    read_count = fread(l->buffer, sizeof(char), READ_BUFFER, l->fh);
+    l->index = -1;
+    l->eof = read_count - 1;
+  }
+
+  l->index++;
+  return l->buffer[l->index];
+}
 
 int main(int argc, char **argv) {
   FILE *fd;
   char c;
-  char buffer[1024] = "";
-  token *tok;
   token_list *tokens;
-  int index = 0;
 
   if (argc < 1 || ((fd = fopen(argv[0], "r")) == NULL)) {
     fprintf(stderr, "usage: lexer file.lox\n");
     exit(64);
   }
 
-  while ((c = getc(fd)) != EOF) {
-    tok = malloc(sizeof(token));
+  int line = 0;
+  char buffer[READ_BUFFER] = "";
+  lexer *l;
+  l->buffer = buffer;
+  l->fh = fd;
+
+  while (1) {
+    c = advance(l);
 
     switch (c) {
-    case '(': tok->t = LEFT_PAREN; break;
-    case ')': tok->t = RIGHT_PAREN; break;
-    case '{': tok->t = LEFT_BRACE; break;
-    case '}': tok->t = RIGHT_BRACE; break;
-    case ',': tok->t = COMMA; break;
-    case '.': tok->t = DOT; break;
-    case '-': tok->t = MINUS; break;
-    case '+': tok->t = PLUS; break;
-    case ';': tok->t = SEMICOLON; break;
-    case '*': tok->t = STAR; break;
+    case '(': add_token(tokens, LEFT_PAREN, line); break;
+    case ')': add_token(tokens, RIGHT_PAREN, line); break;
+    case '{': add_token(tokens, LEFT_BRACE, line); break;
+    case '}': add_token(tokens, RIGHT_BRACE, line); break;
+    case ',': add_token(tokens, COMMA, line); break;
+    case '.': add_token(tokens, DOT, line); break;
+    case '-': add_token(tokens, MINUS, line); break;
+    case '+': add_token(tokens, PLUS, line); break;
+    case ';': add_token(tokens, SEMICOLON, line); break;
+    case '*': add_token(tokens, STAR, line); break;
+
+    case '!':
+      add_token(tokens, (match(l, '=')) ? BANG_EQUAL : BANG, line);
+      break;
+    case '=':
+      add_token(tokens, (match(l, '=')) ? EQUAL_EQUAL : EQUAL, line);
+      break;
+    case '<':
+      add_token(tokens, (match(l, '=')) ? LESS_EQUAL : LESS, line);
+      break;
+    case '>':
+      add_token(tokens, (match(l, '=')) ? GREATER_EQUAL : GREATER, line);
+      break;
+    case '/':
+      if (match(l, '/')) {
+        while (peek(l) != '\n' && !eof(l)) advance(l);
+      } else {
+        add_token(tokens, SLASH, line);
+      }
+      break;
+
+    case ' ': break;
+    case '\r': break;
+    case '\n': line++; break;
+
+    case '"':
+      while (peek(l) != '"' && !eof(l)) {
+        c = advance(l);
+        if (c == '\n') line++;
+      }
     }
-    
   }
 
   fclose(fd);
